8648d7c6882b9613fb5d496411740d6c10c6dcea
= <nassiromid@gmail.com>
2020-11-10 21:39:39 -0800

    Added contact form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+
 import About from './components/about/About';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
     <div className="app">
     <div className="topSection">
         <About />
-
+        <Contact />
     </div>
     </div>
   );
---
+++ src/components/contact/Contact.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './Contact.css';
+
+import { Button, FormControl, TextareaAutosize, TextField } from '@material-ui/core';
+
+const Contact = () => {
+    
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [comments, setComments] = useState('');
+
+    return (
+
+    <div className="contact">
+        <p>Email Me</p>
+        <form>
+            <FormControl>
+                <TextField value={firstName} label='First Name' onChange={e => setFirstName(e.target.value)} />
+            </FormControl>
+            <FormControl>
+                <TextField value={lastName} label='Last Name' onChange={e => setLastName(e.target.value)} />
+            </FormControl>
+            <FormControl>
+                <TextField value={email} label='Email' onChange={e => setEmail(e.target.value)} />
+            </FormControl>
+            <FormControl>
+                <TextField value={phone} label='Phone' onChange={e => setPhone(e.target.value)} />
+            </FormControl>
+            <FormControl>
+            <TextareaAutosize cols={30} rowsMin={4} placeholder="Comments" />
+            </FormControl>
+            <Button>Send</Button>
+        </form>
+    </div>
+    )
+}
+
+export default Contact;
