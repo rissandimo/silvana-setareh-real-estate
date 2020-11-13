cd67dbf5c36ec2b36c33f43c5cca5fda9b4bb92c
= <nassiromid@gmail.com>
2020-11-12 20:03:50 -0800

    Updated footer font

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,6 +3,7 @@ import './Footer.css';
 
 import BusinessIcon from '@material-ui/icons/Business';
 import PhoneIcon from '@material-ui/icons/Phone';
+import ScheduleIcon from '@material-ui/icons/Schedule';
 
 const Footer = () => {
     return (
@@ -16,24 +17,12 @@ const Footer = () => {
                 <BusinessIcon fontSize='large' className='footer__font' />
             </div>
             <div className="section__details">
-            <span>Silvana Saidian</span>
-                    <span>123 N. Palm Dr</span>
-                    <span>Beverly Hills, CA 90210</span>
-                    <span>DRE# 123456</span>
+                <span>SILVANA SAIDIAN</span>
+                <span>123 N. PALM DR</span>
+                <span>BEVERLY HILLS, CA 90210</span>
+                <span>DRE# 123456</span>
             </div>
         </div>
-
-            <div className="footer__section">
-                <div className="section__image">
-                    <BusinessIcon fontSize='large' className='footer__font'/>
-                </div>
-                <div className="section__details">
-                    <span>Silvana Saidian</span>
-                    <span>123 N. Palm Dr</span>
-                    <span>Beverly Hills, CA 90210</span>
-                    <span>DRE# 123456</span>
-                </div>
-            </div>
 
             <div className="footer__section">
                 <div className="section__image">
@@ -42,6 +31,17 @@ const Footer = () => {
                 <div className="section__details">
                     <span>TELEPHONE</span>
                     <span>(310) 666-4744</span>
+                </div>
+            </div>
+
+            <div className="footer__section">
+                <div className="section__image">
+                    <ScheduleIcon fontSize='large' className='footer__font'/>
+                </div>
+                <div className="section__details">
+                    <span>HOURS</span>
+                    <span>MON - FRI</span>
+                    <span>10:00AM - 8:30PM</span>
                 </div>
             </div>
         </div>
