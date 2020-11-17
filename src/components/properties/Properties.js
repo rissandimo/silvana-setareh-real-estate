20a595cb490385c00875e1182fecaa2b6fe51ec7
= <nassiromid@gmail.com>
2020-11-17 10:16:31 -0800

    Changed property names

--- src/components/properties/Properties.js
+++ src/components/properties/Properties.js
@@ -10,7 +10,44 @@ const Properties = () => (
         <h2>Featured Listings</h2>
 
         <div className="properties__group">
+            <div className="property">
+                <img src={houseImage} className="property__image"/>
+                <div className="property__details">
+                    <span className="property__details-price">OFFERED AT $1,000,000</span>
+                    <h4 className="property__details-address">123 N PALM DR</h4>
+                    <span className="property__details-city">BEVERLY HILLS, CA</span>
+                </div>
+            </div>
+            <div className="property">
+                <img src={houseImage} className="property__image"/>
+                <div className="property__details">
+                    <span className="property__details-price">OFFERED AT $1,500,000</span>
+                    <h4 className="property__details-address">222 N ROBERTSON BLVD</h4>
+                    <span className="property__details-city">BEVERLY HILLS, CA</span>
+                </div>
+            </div>
+        </div>
 
+        <div className="properties__group">
+            <div className="property">
+                <img src={houseImage} className="property__image"/>
+                <div className="property__details">
+                    <span className="property__details-price">OFFERED AT $5,000,000</span>
+                    <h4 className="property__details-address">123 N OAKHURST DR</h4>
+                    <span className="property__details-city">BEVERLY HILLS, CA</span>
+                </div>
+            </div>
+            <div className="property">
+                <img src={houseImage} className="property__image"/>
+                <div className="property__details">
+                    <span className="property__details-price">OFFERED AT $8,500,000</span>
+                    <h4 className="property__details-address">555 N GALE DR</h4>
+                    <span className="property__details-city">LOS ANGELES, CA</span>
+                </div>
+            </div>
+        </div>
+
+        <div className="properties__group">
             <div className="property">
                 <img src={houseImage} className="property__image"/>
                 <div className="property__details">
