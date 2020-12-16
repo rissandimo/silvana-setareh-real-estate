bf0d31b133c52eca9933248acb1cf5b47140cb7e
Omid Nassir <nassiromid@gmail.com>
2020-12-16 14:36:51 -0800

    Added style and animations to city imagse

--- src/components/sold/Sold.js
+++ src/components/sold/Sold.js
@@ -6,23 +6,38 @@ import houseImage2 from '../../assets/houses/house-2.jpg';
 
 import beverlyHills from '../../assets/cities/bh.jpg';
 import encino from '../../assets/cities/encino.JPG';
-import losAngeles from '../../assets/cities/la.png';
+import losAngeles from '../../assets/cities/la.jpg';
 import westHollywood from '../../assets/cities/wh.jpg';
 
 const Sold = () => (
     <div className="sold">
 
         <div className="cities">
+
             <div className="city">
+                <div className="city-info">
+                    <h4>Beverly Hills</h4>
+                </div>
                 <img src={beverlyHills} alt=""/>
             </div>
+
             <div className="city">
+                <div className="city-info">
+                    <h4>Encino</h4>
+                </div>
                 <img src={encino} alt=""/>
             </div>
+
             <div className="city">
+                <div className="city-info">
+                    <h4>Los Angeles</h4>
+                </div>
                 <img src={losAngeles} alt=""/>
             </div>
             <div className="city">
+                <div className="city-info">
+                    <h4>West Hollywood</h4>
+                </div>
                 <img src={westHollywood} alt=""/>
             </div>
         </div>
