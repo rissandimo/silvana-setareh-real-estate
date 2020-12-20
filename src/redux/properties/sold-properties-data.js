63a8e322d72652075f0682c443327a8366546bb6
Omid Nassir <nassiromid@gmail.com>
2020-12-20 09:47:35 -0800

    Added 2 more houses

--- src/redux/properties/sold-properties-data.js
+++ src/redux/properties/sold-properties-data.js
@@ -1,4 +1,6 @@
 import house1 from '../../assets/houses/walden.jpg';
+import house2 from '../../assets/houses/2.jpg';
+import house3 from '../../assets/houses/3.jpg';
 
 const SOLD_PROPERTIES_DATA = [
     {
@@ -18,52 +20,26 @@ const SOLD_PROPERTIES_DATA = [
             mls: 13645833
         },
         {
-          id: 2,
-          name: 'Blue Beanie',
-          imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
-          price: 18
+           id: 2,
+           address: '1241 Beverly View Dr. Beverly Hills, CA 90211',
+           price: 182500,
+           bedrooms: 4,
+           bathrooms: 4,
+           image: house2,
+           sqft: 2848,
+           type: 'Residential',
+           mls: 9393561
         },
         {
           id: 3,
-          name: 'Brown Cowboy',
-          imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
-          price: 35
-        },
-        {
-          id: 4,
-          name: 'Grey Brim',
-          imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
-          price: 25
-        },
-        {
-          id: 5,
-          name: 'Green Beanie',
-          imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
-          price: 18
-        },
-        {
-          id: 6,
-          name: 'Palm Tree Cap',
-          imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
-          price: 14
-        },
-        {
-          id: 7,
-          name: 'Red Beanie',
-          imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
-          price: 18
-        },
-        {
-          id: 8,
-          name: 'Wolf Cap',
-          imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
-          price: 14
-        },
-        {
-          id: 9,
-          name: 'Blue Snapback',
-          imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
-          price: 16
+          address: '466 S Holt Ave. Los Angeles, CA 90048',
+          price: 1795000,
+          bedrooms: 4,
+          bathrooms: 4,
+          image: house3,
+          sqft: 2563,
+          type: 'Residential',
+          mls: 17230202
         }
       ]
     }
