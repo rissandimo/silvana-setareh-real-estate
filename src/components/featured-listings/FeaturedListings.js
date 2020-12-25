566c2f745c93fe3880219262c10d10f4cf747eaf
Omid Nassir <nassiromid@gmail.com>
2020-12-24 20:17:17 -0800

    Able to update next arrow count

--- src/components/featured-listings/FeaturedListings.js
+++ src/components/featured-listings/FeaturedListings.js
@@ -1,5 +1,5 @@
-import { Button } from '@material-ui/core';
-import React from 'react';
+import { Button } from '@material-ui/core'; // Create custom button
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components/macro';
 import { css } from 'styled-components';
 import { IoMdArrowRoundForward } from 'react-icons/io';
@@ -26,11 +26,73 @@ const ListingsWrapper = styled.div`
 `;
 
 // Listings Slider
-const ListingSlide = styled.div``;
-const ListingSlider = styled.div``;
-const ListingImage = styled.img``;
-const ListingContent = styled.div``;
-const Arrow = styled(IoMdArrowRoundForward)``;
+const ListingSlide = styled.div`
+height: 100%;
+width: 100%;
+z-index: 1;
+`;
+const ListingSlider = styled.div`
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &::before {
+        content: '';
+        position: absolute;
+        z-index: 2;
+        width: 100%;
+        height: 100vh;
+        bottom: 0vh;
+        left: 0;
+        overflow: hidden;
+        opacity: 0.4;
+        background: linear-gradient(
+            0deg,
+            rgba(0,0,0,0.2) 0%,
+            rgba(0,0,0,0.2) 50%,
+            rgba(0,0,0,0.2) 100%,
+        );
+    }
+`;
+const ListingImage = styled.img`
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    object-fit: cover;
+`;
+const ListingContent = styled.div`
+position: relative;
+z-index: 10;
+display: flex;
+flex-direction: column;
+max-width: 1600px;
+width: calc(100% - 100px);
+color: #fff;
+
+h1 {
+    font-size: clamp(1rem, 8vw, 2rem);
+    font-width: 400;
+    text-transform: uppercase;
+    text-shadow: 0px 0px 20px rgba(0,0,0,0.4);
+    text-align: left;
+    margin-bottom: 0.8rem;
+}
+
+p {
+    margin-bottom: 1.2rem;
+    text-shadow: 0px 0px 20px rgba(0,0,0,0.4);
+}
+`;
+const Arrow = styled(IoMdArrowRoundForward)`
+margin-left: 0.5rem
+`;
 
 const arrowButtons = css`
     background: #000d1a;
@@ -58,16 +120,26 @@ const SliderButtons = styled.div`
     z-index: 10;
 `;
 
-const NextArrow = styled(IoArrowBack)`
+const NextArrow = styled(IoArrowForward)`
     ${arrowButtons}
 `
 
-const PrevArrow = styled(IoArrowForward)`
+const PrevArrow = styled(IoArrowBack)`
     ${arrowButtons}
 `
 
 const FeaturedListings = () => {
-    console.log(SliderData);
+    
+    const [current, setCurrent] = useState(0);
+    const length = SliderData.length;
+    const timeout = useRef(null);
+
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1);
+        console.log(current);
+    }
+
+
     return(
     <FeaturedListingsSection>
             <ListingsWrapper>
@@ -75,7 +147,7 @@ const FeaturedListings = () => {
                 {SliderData.map((slide, index) => (
                     <ListingSlide key={index}>
                         <ListingSlider>
-                            <ListingImage/>
+                            <ListingImage src={slide.image} alt={slide.alt}/>
                             <ListingContent>
                                 <h1>{slide.title}</h1>
                                 <p>{slide.price}</p>
@@ -89,8 +161,8 @@ const FeaturedListings = () => {
                     </ListingSlide>
                 ))}
                 <SliderButtons>
-                    <NextArrow />
                     <PrevArrow />
+                    <NextArrow onClick={nextSlide} />
                 </SliderButtons>
             </ListingsWrapper>
     </FeaturedListingsSection>  
