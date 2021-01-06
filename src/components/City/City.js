635df5a96192dac61fb2fdb282591e244e42b4e1
Omid Nassir <nassiromid@gmail.com>
2021-01-05 19:02:25 -0800

    Added sold houses quantity

--- src/components/City/City.js
+++ src/components/City/City.js
@@ -8,6 +8,11 @@ const City = ({ title, imageUrl, linkUrl }) => (
     <div className="city-info">
         <h4>{title}</h4>
     </div>
+    <div className="houses__sold">
+        <p>
+            8
+        </p>
+    </div>
     <Link to={linkUrl} className="city__link">
         <img src={imageUrl} alt=""/>
     </Link>
