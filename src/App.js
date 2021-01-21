cd148f549a3174d43e06cb54ec0d7d89d1a71a89
Omid Nassir <nassiromid@gmail.com>
2021-01-20 17:38:06 -0800

    Added additional media queries - iphone

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
       <Route path='/sold' component={Sold} />
     </Switch>
     
-    <Footer />
+    {/* <Footer /> */}
     </div>
   );
 }
--- src/components/contact-details/ContactDetails.js
+++ src/components/contact-details/ContactDetails.js
@@ -9,35 +9,35 @@ import TwitterIcon from '@material-ui/icons/Twitter';
 
 const ContactDetails = () => (
     <div className="contact__details">
-    <h5>CONTACT DETAILS</h5>
-    <p className="telephone">Tel: (310) 625-2123</p>
-    <p className="telephone">Fax: (310) 471-2602</p>
+        <h5>CONTACT DETAILS</h5>
+        <p className="telephone">Tel: (310) 625-2123</p>
+        <p className="telephone">Fax: (310) 471-2602</p>
 
-    <div className="address">
-        <p>Mon - Friday, 9 am - 6 pm</p>
-        <p>
-        9197 Wilshire Blvd. Suite 321 <br />
-        Bevely Hills, CA 90210
-        </p>
-    </div>
+        <div className="address">
+            <p>Mon - Friday, 9 am - 6 pm</p>
+            <p>
+            9197 Wilshire Blvd. Suite 321 <br />
+            Bevely Hills, CA 90210
+            </p>
+        </div>
 
-    <div className="agency">
-        <p>REALTY REALTORS</p>
-        <p>Beverly Hills Real Estate Agent</p>
-        <p>DRE #2134567</p>
-    </div>
+        <div className="agency">
+            <p>REALTY REALTORS</p>
+            <p>Beverly Hills Real Estate Agent</p>
+            <p>DRE #2134567</p>
+        </div>
 
-    <div className="social">
-        <a href="#" className="link">
-            <FacebookIcon className="icon" />
-        </a>
-        <a href="#" className="link">
-            <InstagramIcon />
-        </a>
-        <a href="https://www.linkedin.com/in/soraya-setareh-3a781625/" target="_blank" className="link">
-            <TwitterIcon />
-        </a>
-    </div>
+        <div className="social">
+            <a href="#" className="link">
+                <FacebookIcon className="icon" />
+            </a>
+            <a href="#" className="link">
+                <InstagramIcon />
+            </a>
+            <a href="https://www.linkedin.com/in/soraya-setareh-3a781625/" target="_blank" className="link">
+                <TwitterIcon />
+            </a>
+        </div>
 
 </div>
 )
